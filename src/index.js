259a13f0530a4295f33f46f3118a8cd807191929
pariwit12 <pariwit12@hotmail.com>
2025-09-25 16:41:14 +0700

    add PWA installable

--- src/index.js
+++ src/index.js
@@ -4,12 +4,20 @@ import './index.css';
 import App from './App.jsx';
 import reportWebVitals from './reportWebVitals';
 
+// สำหรับ PWA
+// ✅ เพิ่ม import สำหรับ Service Worker
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+// สำหรับ PWA
+// ✅ Register Service Worker (online-first)
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
